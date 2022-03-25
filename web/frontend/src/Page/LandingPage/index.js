079361b12210bc29f1846aef803fce0aeca0d843
JIH0LEE <skql5810@gmail.com>
2022-03-25 18:10:53 +0900

    스타일 방법 바구기

--- web/frontend/src/Page/LandingPage/index.js
+++ web/frontend/src/Page/LandingPage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 const LandingPage = () => {
   return (
-    <Container className="main-container">
+    <Container className="landing-container">
       <div>메인 페이지</div>
     </Container>
   )
