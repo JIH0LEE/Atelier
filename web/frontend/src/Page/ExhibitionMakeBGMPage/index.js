3907388da97024adf3677eb86b501be78085f357
JIH0LEE <skql5810@gmail.com>
2022-05-06 00:02:07 +0900

    bgm change

--- web/frontend/src/Page/ExhibitionMakeBGMPage/index.js
+++ web/frontend/src/Page/ExhibitionMakeBGMPage/index.js
@@ -1,92 +1,125 @@
 import React, { useState } from 'react'
-import {
-  Container,
-  Tab,
-  Col,
-  Button,
-  ListGroup,
-  Tabs,
-  Row,
-} from 'react-bootstrap'
+import { Container, Tab, Col, Button, ListGroup, Row } from 'react-bootstrap'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import ReactAudioPlayer from 'react-audio-player'
 
 const ExhibitionMakeBGMPage = () => {
+  const [currentBGM, setCurrentBGM] = useState(null)
   const cuteBGM = [
     {
+      id: 1,
       name: 'Jay Jay - Kevin MacLeod',
       src: 'http://docs.google.com/uc?export=open&id=1PyIHSBI2juBwoub3P6m9j65fjP02OhhV',
     },
     {
+      id: 2,
       name: 'Jigsaw Puzzle - The Green Orbs',
       src: 'http://docs.google.com/uc?export=open&id=1offo2TGHUXksn3ok6cVDrQPKhoKH8ahz',
     },
     {
+      id: 3,
       name: 'Lovable Clown Sit Com - Sir Cubworth',
       src: 'http://docs.google.com/uc?export=open&id=1OpZSwINyehkj5Sqg0is2XVYusLwAz2mL',
     },
     {
+      id: 4,
       name: 'Springtime Family Band - The Green Orbs',
       src: 'http://docs.google.com/uc?export=open&id=14OobcKwHB27HTUKAyrGlceGgfVNmsd1k',
     },
   ]
   const calmBGM = [
     {
+      id: 1,
       name: 'Arms of Heaven - Aakash Gandhi',
       src: 'http://docs.google.com/uc?export=open&id=10bWZZmjaWL9BN5MCpmYzIHUuP0r69ubd',
     },
     {
+      id: 2,
       name: 'Beseeched - Asher Fulero',
       src: 'http://docs.google.com/uc?export=open&id=1DEjWLrY3ZZfjy8ADRHc9jQoMXG4vz-jd',
     },
     {
+      id: 3,
       name: 'Kiss the Sky - Aakash Gandhi',
       src: 'http://docs.google.com/uc?export=open&id=1H4byx9DXMjeQg0X6dvh7R0oC1QOePcPS',
     },
     {
+      id: 4,
       name: 'One Step Closer - Aakash Gandhi',
       src: 'http://docs.google.com/uc?export=open&id=1lRtla5YKHDrvTQITu3LNE5WOhIz2gPnL',
     },
   ]
   const excitingBGM = [
     {
+      id: 1,
       name: 'Alternate - Vibe Tracks',
       src: 'http://docs.google.com/uc?export=open&id=1TpxtKMk9O-l4j3lu2a48TiUhgnr5A7NU',
     },
     {
+      id: 2,
       name: 'Beat Your Competition - Vibe Tracks',
       src: 'http://docs.google.com/uc?export=open&id=1-m6M-5Ep2V_B90ljaXQB19ZB4RD9rBQH',
     },
     {
+      id: 3,
       name: 'Classique - Francis Preve',
       src: 'http://docs.google.com/uc?export=open&id=1fNCsN6smff1u0aPm1LRivlljHAtKSiYk',
     },
     {
+      id: 4,
       name: 'Down with Paradise - Norma Rockwell.',
       src: 'http://docs.google.com/uc?export=open&id=1GCTJ0wiJ9oBgFR8hAHhUYNqgq6CjABgZ',
     },
   ]
-  const bightBGM = [
+  const brightBGM = [
     {
+      id: 1,
       name: 'Bike Rides - The Green Orbs',
       src: 'http://docs.google.com/uc?export=open&id=12GbzjUB0f99yZy7JJhWHdPXLwaKUq8Ra',
     },
     {
+      id: 2,
       name: 'Cha Cappella - Jimmy Fontanez_Media Right Productions',
       src: 'http://docs.google.com/uc?export=open&id=1ke-cmTZRGpEtDwjOQv1pfh8CKbNhqzLY',
     },
     {
+      id: 3,
       name: 'If I Had a Chicken - Kevin MacLeod',
       src: 'http://docs.google.com/uc?export=open&id=1AZofsqr9BHZbMIXAz2-vNaWWeazF1YNd',
     },
     {
+      id: 4,
       name: 'Payday - Jason Farnham',
       src: 'http://docs.google.com/uc?export=open&id=1Vmm672LVzoRFaShOwt1WGujDh0_87Kp0',
     },
   ]
-  const changeBGM = e => {
-    console.log(e.target.key)
+  function isIdValid(element) {
+    if (element.id === 'apple') {
+      return true
+    }
+  }
+  const changeCuteBGM = e => {
+    let findId = e.target.id
+    let obj = cuteBGM.find(bgm => bgm.id == findId)
+    document.getElementById('myAudio').volume = 0.1
+    setCurrentBGM(obj.src)
+  }
+  const changeCalmBGM = e => {
+    let findId = e.target.id
+    let obj = calmBGM.find(bgm => bgm.id == findId)
+    document.getElementById('myAudio').volume = 0.1
+    setCurrentBGM(obj.src)
+  }
+  const changeExcitingBGM = e => {
+    let findId = e.target.id
+    let obj = excitingBGM.find(bgm => bgm.id == findId)
+    document.getElementById('myAudio').volume = 0.1
+    setCurrentBGM(obj.src)
+  }
+  const changeBrightBGM = e => {
+    let findId = e.target.id
+    let obj = brightBGM.find(bgm => bgm.id == findId)
+    document.getElementById('myAudio').volume = 0.1
+    setCurrentBGM(obj.src)
   }
 
   return (
@@ -108,12 +141,11 @@ const ExhibitionMakeBGMPage = () => {
         </Container>
         <Container id="elem2">
           <audio
+            id="myAudio"
             controls
-            id="bgm"
+            volume={0.5}
             loop
-            src={
-              'http://docs.google.com/uc?export=open&id=1PyIHSBI2juBwoub3P6m9j65fjP02OhhV'
-            }
+            src={currentBGM}
             autoPlay={true}
           ></audio>
         </Container>
@@ -123,16 +155,16 @@ const ExhibitionMakeBGMPage = () => {
               <Col sm={4}>
                 <Container className="inner1">
                   <ListGroup>
-                    <ListGroup.Item action eventKey="#1">
+                    <ListGroup.Item action eventKey="cute">
                       귀여운
                     </ListGroup.Item>
-                    <ListGroup.Item action eventKey="#2">
+                    <ListGroup.Item action eventKey="calm">
                       잔잔한
                     </ListGroup.Item>
-                    <ListGroup.Item action eventKey="#3">
+                    <ListGroup.Item action eventKey="exciting">
                       신나는
                     </ListGroup.Item>
-                    <ListGroup.Item action eventKey="#4">
+                    <ListGroup.Item action eventKey="bright">
                       밝은
                     </ListGroup.Item>
                   </ListGroup>
@@ -140,18 +172,61 @@ const ExhibitionMakeBGMPage = () => {
               </Col>
               <Col sm={8}>
                 <Tab.Content>
-                  <Tab.Pane eventKey="#1">
+                  <Tab.Pane eventKey="cute">
                     <ListGroup>
                       {cuteBGM.map(bgm => (
-                        <ListGroup.Item onClick={changeBGM}>
+                        <ListGroup.Item
+                          action
+                          id={bgm.id}
+                          onClick={changeCuteBGM}
+                        >
                           {bgm.name}
                         </ListGroup.Item>
                       ))}
                     </ListGroup>
                   </Tab.Pane>
-                  <Tab.Pane eventKey="#2">2</Tab.Pane>
-                  <Tab.Pane eventKey="#3">3</Tab.Pane>
-                  <Tab.Pane eventKey="#4">4</Tab.Pane>
+                  <Tab.Pane eventKey="calm">
+                    {' '}
+                    <ListGroup>
+                      {calmBGM.map(bgm => (
+                        <ListGroup.Item
+                          action
+                          id={bgm.id}
+                          onClick={changeCalmBGM}
+                        >
+                          {bgm.name}
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="exciting">
+                    {' '}
+                    <ListGroup>
+                      {excitingBGM.map(bgm => (
+                        <ListGroup.Item
+                          action
+                          id={bgm.id}
+                          onClick={changeExcitingBGM}
+                        >
+                          {bgm.name}
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="bright">
+                    {' '}
+                    <ListGroup>
+                      {brightBGM.map(bgm => (
+                        <ListGroup.Item
+                          action
+                          id={bgm.id}
+                          onClick={changeBrightBGM}
+                        >
+                          {bgm.name}
+                        </ListGroup.Item>
+                      ))}
+                    </ListGroup>
+                  </Tab.Pane>
                 </Tab.Content>
               </Col>
             </Row>
