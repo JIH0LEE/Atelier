109f95ef1f8050ec00aa2d82df26bd85bf4f7aff
tmddus2 <tmddus2__@naver.com>
2022-05-10 17:51:35 +0900

    hahaha

--- web/frontend/src/Page/ExhibitionMake2stepPage/index.js
+++ web/frontend/src/Page/ExhibitionMake2stepPage/index.js
@@ -60,44 +60,41 @@ const ExhibitionMake2stepPage = ({ id }) => {
         })
 
     }
-    axios.post('/api/user/make-exhibition-step2', formData).then(res => {
-        console.log(res)
-    })
-}
 
-const next = () => { }
 
-return (
-    <Container className="exhibition_make-container2">
-        <Container className="inner">
-            <Container id="elem1">
-                <Col>
-                    <ListGroup horizontal>
-                        <ListGroup.Item>Step1</ListGroup.Item>
-                        <ListGroup.Item active>Step2</ListGroup.Item>
-                        <ListGroup.Item>Step3</ListGroup.Item>
-                        <ListGroup.Item>Step4</ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col>
-                    <div className="title">게시물 올리기</div>
-                </Col>
-                <Col></Col>
-            </Container>
+    const next = () => { }
 
-            <Container id="elem3">
-                <ExhibitionPosting
-                    postList={postList}
-                    func={getPostList}
-                ></ExhibitionPosting>
-            </Container>
-            <Container id="elem4">
-                <Button onClick={save}>Save</Button>
-                <Button onClick={next}>Next</Button>
+    return (
+        <Container className="exhibition_make-container2">
+            <Container className="inner">
+                <Container id="elem1">
+                    <Col>
+                        <ListGroup horizontal>
+                            <ListGroup.Item>Step1</ListGroup.Item>
+                            <ListGroup.Item active>Step2</ListGroup.Item>
+                            <ListGroup.Item>Step3</ListGroup.Item>
+                            <ListGroup.Item>Step4</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col>
+                        <div className="title">게시물 올리기</div>
+                    </Col>
+                    <Col></Col>
+                </Container>
+
+                <Container id="elem3">
+                    <ExhibitionPosting
+                        postList={postList}
+                        func={getPostList}
+                    ></ExhibitionPosting>
+                </Container>
+                <Container id="elem4">
+                    <Button onClick={save}>Save</Button>
+                    <Button onClick={next}>Next</Button>
+                </Container>
             </Container>
         </Container>
-    </Container>
-)
+    )
 }
 
 export default ExhibitionMake2stepPage
