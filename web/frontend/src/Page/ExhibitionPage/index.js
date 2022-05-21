5b7dd72b4d01937c2d6f0f28445d9198c1e69456
JIH0LEE <skql5810@gmail.com>
2022-05-21 22:03:09 +0900

    bak

--- web/frontend/src/Page/ExhibitionPage/index.js
+++ web/frontend/src/Page/ExhibitionPage/index.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Container } from 'react-bootstrap'
 import { useLocation } from 'react-router-dom'
+import Background from '../ExhibitionMake4stepPage/Background'
+import background1 from '../../images/exhibitionBackground/background1.jpg'
+import background2 from '../../images/exhibitionBackground/background2.jpg'
+import background3 from '../../images/exhibitionBackground/background3.jpg'
+import background4 from '../../images/exhibitionBackground/background4.jpg'
 import axios from 'axios'
 
 import './style.css'
@@ -8,10 +13,25 @@ import './style.css'
 const ExhibitionPage = () => {
   const location = useLocation()
   const [id, setId] = useState(location.state.id)
+  const [contents, setContents] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`/api/select-online-exhibition?id=${id}`)
+      setContents(res.data.contents)
+    }
+
+    fetchData()
+  }, [])
+
   console.log(id)
   return (
     <Container className="exhibition-show-container">
-      <Container className="inner"></Container>
+      <Container className="inner">
+        <Background
+          imgSrc={background4}
+          classNameParm={'background4'}
+        ></Background>
+      </Container>
     </Container>
   )
 }
