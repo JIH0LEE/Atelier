16b83a7ea91fb91f8cd097e512e662a10b80caf9
JIH0LEE <skql5810@gmail.com>
2022-05-24 01:26:30 +0900

    오프라인 상세보기 작업중

--- web/frontend/src/Page/OfflineExhibitionInfo/index.js
+++ web/frontend/src/Page/OfflineExhibitionInfo/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import {
   Button,
@@ -13,16 +14,31 @@ import { useParams } from 'react-router-dom'
 import './style.css'
 const OfflineExhibitionInfo = () => {
   const { id } = useParams()
-  console.log(id)
-  useEffect(() => {}, [])
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [link, setLink] = useState('')
+  const [poster, setPoster] = useState('')
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`/api/get-offline-info?id=${id}`)
+      setTitle(res.data.title)
+      setDescription(res.data.descript)
+      setLink(res.data.link)
+      setPoster(res.data.poaster)
+    }
+
+    fetchData()
+  }, [])
 
   return (
-    //console.log(parms.key)
-    <Container>
-      <Container
-        className="exhibitionInfo-container"
-        style={{ padding: '5px', display: 'block' }}
-      ></Container>
+    <Container className="offline-exhibitionInfo-container">
+      <Container className="title">{title}</Container>
+      <Container className="body">
+        <Container className="poaster"></Container>
+        <Container className="descript"></Container>
+      </Container>
+      <Container className="link">{link}</Container>
     </Container>
   )
 }
