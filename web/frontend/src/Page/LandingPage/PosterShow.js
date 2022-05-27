7a6fcbfa569217d8d99b9ea1fb1d076d068cac0d
tmddus2 <tmddus2__@naver.com>
2022-05-27 14:13:20 +0900

    main page

--- web/frontend/src/Page/LandingPage/PosterShow.js
+++ web/frontend/src/Page/LandingPage/PosterShow.js
@@ -1,23 +1,44 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Container, Figure, Carousel } from 'react-bootstrap'
 
 const PosterShow = () => {
+    const navigate = useNavigate()
     const [exhibitions, setExhibitions] = useState(null)
-
+    //const navigate = useNavigate()
     useEffect(() => {
         axios.get("/api/get-top-exhibition").then(res => {
             setExhibitions(res.data)
             console.log(res.data)
         })
+
     }, [])
+
+    const ToExhibitionPage = (parms) => {
+        console.log(parms)
+        //const navigate = useNavigate()
+        navigate(`/exhibition/${parms.id}`, {
+            state: {
+                id: parms.id,
+                title: parms.title,
+                date: parms.endDate,
+                keyword: [parms.tag1, parms.tag2, parms.tag3],
+                poaster: parms.poster,
+                description: parms.description,
+                like: parms.like_count,
+                //author: parms.author,
+            },
+        })
+
+    }
 
     return (<>
         <Container>
             <Carousel>
                 {exhibitions ?
                     exhibitions.map(exhibition => (
-                        <Carousel.Item>
+                        <Carousel.Item onClick={() => ToExhibitionPage(exhibition)}>
                             <Container className="content">
                                 <Container><Figure.Image src={exhibition.poster}></Figure.Image></Container>
                             </Container>
